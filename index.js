a8be4dc30c982c4306fa676aed5812e66e173dcc
Brian Donohue <brian.donohue89@gmail.com>
2015-09-19 13:18:15 -0400

    Implemented Venmo parameter parser
    Signed-off-by: Brian Donohue <brian.donohue89@gmail.com>

--- index.js
+++ index.js
@@ -6,8 +6,36 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var message = step.input('message').first();
+        var parts = message.split(' ');
+        var username = undefined;
+        var amount = undefined;
+        var self = this;
+        for (var i = 0; i < parts.length; i++) {
+            if (parts[i][0] == '@' && parts[i].length > 1) {
+                username = parts[i].substr(1);
+            }
+            else if (parts[i][0] == '$' && parts[i].length > 1) {
+                amount = parseInt(parts[i].substr(1));
+            }
+        }
+
+        if (username == undefined || username.length == 0) {
+            return self.fail({
+                message: 'A Venmo username was not found. The username must contain an "@" before it (e.g. @Username)',
+                input: step.inputs()
+            });
+        }
+        else if (amount == undefined || isNaN(amount)) {
+            return self.fail({
+                message: 'A valid amount was not found. The amount must contain a "$" before it (e.g. $20)',
+                input: step.inputs()
+            });
+        }
+
+        self.complete({
+            'username': username,
+            'amount': amount
+        });
     }
 };
